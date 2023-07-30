14f8cf499af8b5995dfa81e0fd444fc2a55cb942
aravinthraajk <aravinthraajk@gmail.com>
2023-07-31 03:23:51 +0530

    feat: card component implemented

---
+++ src/components/atoms/Card/Card.tsx
@@ -0,0 +1,56 @@
+import React from "react"
+import styles from "./Card.module.scss"
+import Image from "next/image"
+import vehicle from "@Images/vehicle.png"
+import Button from "../Button/Button"
+import { StaticImport } from "next/dist/shared/lib/get-img-props"
+import Link from "next/link"
+
+interface CardProps {
+  imageUrl: string | StaticImport
+  weight: string
+  model: string
+  pdfUrl: string
+}
+
+const Card = ({ data }: { data: CardProps }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.imgWrapper}>
+        <Image
+          src={data.imageUrl}
+          alt="vehicle image"
+          fill
+          objectFit="contain"
+        />
+      </div>
+      <div className={styles.detailsWrapper}>
+        <div className={styles.details}>
+          <div className={styles.weight}>{data.weight}</div>
+          <div className={styles.model}>{data.model}</div>
+        </div>
+        <div className={styles.download}>
+          <Link href={data.pdfUrl} target="_blank">
+            <Button>製品詳細（PDF）</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+    // <div className={styles.card}>
+    //   <div className={styles.imgWrapper}>
+    //     <Image src={vehicle} alt="vehicle image" fill objectFit="contain" />
+    //   </div>
+    //   <div className={styles.detailsWrapper}>
+    //     <div className={styles.details}>
+    //       <div className={styles.weight}>400 t</div>
+    //       <div className={styles.model}>タダノ　ATF400G6</div>
+    //     </div>
+    //     <div className={styles.download}>
+    //       <Button>製品詳細（PDF）</Button>
+    //     </div>
+    //   </div>
+    // </div>
+  )
+}
+
+export default Card
