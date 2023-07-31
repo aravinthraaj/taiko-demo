8ed11d9fe304f359e2f348e23fcd6c6382525484
aravinthraajk <aravinthraajk@gmail.com>
2023-07-31 07:17:13 +0530

    feat: hero section implemented

---
+++ src/components/template/HeroSection/HeroSection.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image"
+import React from "react"
+import vehicle from "@Images/vehicle.png"
+import styles from "./HeroSection.module.scss"
+import Logo from "@/components/atoms/Logo/Logo"
+
+const HeroSection = () => {
+  return (
+    <div className={styles.hero}>
+      <Image
+        src={vehicle}
+        alt="background"
+        className={styles.heroimage}
+        fill
+        objectFit="cover"
+      />
+      <div className={styles.overlay}>
+        <Logo />
+        <div className={styles.description}>
+          <div className={styles.pink}>トップクラスの大型重機エキスパート</div>
+          <p>
+            三河エリア随一の総トン数1,800t超＜2023/7月＞を誇り、
+            <br />
+            多様なクレーンニーズにお応えします
+          </p>
+        </div>
+        <div className={styles.scroll}>
+          <div className={styles.text}>scroll</div>
+          <div className={styles.line}></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
