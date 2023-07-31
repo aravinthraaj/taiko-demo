93d11b16aab037c29c5c6571d086a06421de2d27
aravinthraajk <aravinthraajk@gmail.com>
2023-08-01 04:31:51 +0530

    feat: mobile header implemented

--- src/components/atoms/Header/Header.tsx
+++ src/components/atoms/Header/Header.tsx
@@ -1,9 +1,21 @@
-import React from "react"
+"use client"
+import React, { useState } from "react"
 import styles from "./Header.module.scss"
 import Logo from "../Logo/Logo"
 import Link from "next/link"
+import BurgerMenu from "@Icons/burger-menu.svg"
+import Image from "next/image"
+import MobileNav from "../MobileNav/MobileNav"
+import CloseButton from "@Icons/close.svg"
 
 const Header = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+  const closeModal = () => {
+    setNavbarOpen(false)
+  }
+  const toggle = () => {
+    setNavbarOpen((prev) => !prev)
+  }
   return (
     <div className={styles.headerWrapper}>
       <header className={styles.header}>
@@ -14,8 +26,16 @@ const Header = () => {
           <Link href={"/company"}>会社概要</Link>
           <Link href={"/recruit"}>採用情報</Link>
         </nav>
-        <div></div>
+
+        <div className={styles.burgerMenu} onClick={toggle}>
+          {navbarOpen ? (
+            <Image src={CloseButton} alt={"menu"} />
+          ) : (
+            <Image src={BurgerMenu} alt={"menu"} />
+          )}
+        </div>
       </header>
+      <div>{navbarOpen ? <MobileNav toggle={closeModal} /> : null}</div>
     </div>
   )
 }
